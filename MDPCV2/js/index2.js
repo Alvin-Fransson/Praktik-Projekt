5db9eb29a406782221423aab713fb273eddc2870
Alvin Fransson <alvinfransson06@gmail.com>
2025-07-07 20:52:33 +0200

    Delete MDPCV2 directory

--- MDPCV2/js/index2.js
+++
@@ -1,6 +0,0 @@
-$('#jump_to_loc').click(function(){
-    var jump = $(this).attr('href');
-    var new_position = $('#'+jump).offset();
-    window.scrollTo(new_position.left,new_position.top);
-    return false;
-});
